2e3f4ea248efbb73b5d4cdb6a97633b6f55be1db
tabishjamal <tjamal333@gmail.com>
2026-08-12 21:45:57 +0530

    Initial Commit

---
+++ Kaushal Panjee/Kaushal_PanjeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kaushal_PanjeeApp.swift
+//  Kaushal Panjee
+//
+//  Created by Tabish Jamal on 12/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Kaushal_PanjeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
